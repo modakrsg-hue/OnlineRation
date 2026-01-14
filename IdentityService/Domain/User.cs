7f9a663aa660e27abfe5d3525168052c51129105
SantuModak <santumodak.rsg@gmail.com>
2026-01-14 11:19:53 +0530

    Feature Added Identity Feature

---
+++ IdentityService/Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Mobile { get; set; } = string.Empty;
+    public string Role { get; set; } = "Citizen";
+}
--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -1,25 +1,17 @@
+using IdentityService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
-
 app.Run();
